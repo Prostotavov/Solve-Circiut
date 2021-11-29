713ed3f609148374ae3aacd4915892235f60e8b1
Roma Senkevich <84024788+Prostotavov@users.noreply.github.com>
2021-11-29 19:25:49 +0300

    Initial Commit

---
+++ Solve Circiut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Solve Circiut
+//
+//  Created by MacBook Pro on 29.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
